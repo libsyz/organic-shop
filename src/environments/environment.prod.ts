3121ade337fdc4ca0757d8fd7c27917a90c6b463
miguel jimenez <denkatalabeno@gmail.com>
2018-05-23 01:41:41 +0200

    doing well

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+      apiKey: "AIzaSyBybhrdD1e-1LcwXijb6k0w8mP1VZ9vfmc",
+      authDomain: "organic-shop-af935.firebaseapp.com",
+      databaseURL: "https://organic-shop-af935.firebaseio.com",
+      projectId: "organic-shop-af935",
+      storageBucket: "organic-shop-af935.appspot.com",
+      messagingSenderId: "976269434717"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBybhrdD1e-1LcwXijb6k0w8mP1VZ9vfmc",
+    authDomain: "organic-shop-af935.firebaseapp.com",
+    databaseURL: "https://organic-shop-af935.firebaseio.com",
+    projectId: "organic-shop-af935",
+    storageBucket: "organic-shop-af935.appspot.com",
+    messagingSenderId: "976269434717"
+  }
 };
 
 /*
